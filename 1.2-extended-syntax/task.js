67816e27659b6967fa45ac260935bb0d83b242c5
Vasiliy Merzlyakov <V.Merov@yandex.ru>
2020-07-05 21:15:16 +0500

    second task, first try

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,38 @@
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    "use strict";
+    let x =[];
+    let d = b**2 - 4*a*c
+    if(d === 0){x.push((-b + Math.sqrt(d))/(2*a))
+    } else if(d > 0){
+        x.push((-b + Math.sqrt(d))/(2*a))
+        x.push((-b - Math.sqrt(d))/(2*a))
+    } else if(d < 0){
+      x = 0
+    }
+     return x;
 }
 
-function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+function getAverageMark(marks){   
+    let a = 0; 
+    if(marks.length){
+      if(marks.length > 5){
+        console.log("Оценок больше 5")}
+        marks.splice(5)  
+        for(let i = 0; i <= marks.length - 1; i++){   
+          a = marks[i] + a   
+    }
+    a = a / marks.length    
+  } else if (marks.length === 0){
+    return 0
+  }  
+      // код для задачи №2 писать здесь
+       return a;
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+    let yearBirtha = dateOfBirthday.getFullYear()
+    let toDay = new Date().getFullYear()
+    let age = toDay - yearBirtha
+    return age >= 18? `Не желаете ли олд-фэшн, ${name}?`: `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`
+    
 }
