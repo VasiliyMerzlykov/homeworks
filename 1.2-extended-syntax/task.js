33ebefde65d83ee46d09721cd7c18f9cff87d7fa
Vasiliy Merzlyakov <V.Merov@yandex.ru>
2020-07-06 13:06:14 +0500

    second task second try

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -2,37 +2,32 @@ function getResult(a,b,c){
     "use strict";
     let x =[];
     let d = b**2 - 4*a*c
-    if(d === 0){x.push((-b + Math.sqrt(d))/(2*a))
+    if(d === 0){
+        x.push((-b + Math.sqrt(d))/(2*a))
     } else if(d > 0){
         x.push((-b + Math.sqrt(d))/(2*a))
         x.push((-b - Math.sqrt(d))/(2*a))
-    } else if(d < 0){
-      x = 0
-    }
-     return x;
+    } else return x;
 }
 
+
+
+//                ***SECOND TRY SECOND TASK*** КОД СТАЛ КОРОЧЕ И ЧИТАБЕЛЬНЕЕ !!! СПАСИБО
 function getAverageMark(marks){   
-    let a = 0; 
-    if(marks.length){
-      if(marks.length > 5){
-        console.log("Оценок больше 5")}
-        marks.splice(5)  
-        for(let i = 0; i <= marks.length - 1; i++){   
-          a = marks[i] + a   
-    }
-    a = a / marks.length    
-  } else if (marks.length === 0){
-    return 0
-  }  
-      // код для задачи №2 писать здесь
+    let sumScores = 0; 
+    if(marks.length === 0){
        return a;
+    } else if(marks.length){
+      marks.splice(5)
+      for(let i = 0; i <= marks.length - 1; i++){   
+        sumScores = marks[i] + a
+      }
+    };    
+    return (sumScores / marks.length).toFixed(1);
 }
 
 function askDrink(name,dateOfBirthday){
-    let yearBirtha = dateOfBirthday.getFullYear()
-    let toDay = new Date().getFullYear()
-    let age = toDay - yearBirtha
+    let age = new Date().getFullYear() - dateOfBirthday.getFullYear() // СПАСИБО ЗА ЭТО ЗАМЕЧАНИЕ, ЭТО РЕАЛЬНО ЧТО-ТО НОВОЕ ДЛЯ МЕНЯ
     return age >= 18? `Не желаете ли олд-фэшн, ${name}?`: `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`
     
 }
