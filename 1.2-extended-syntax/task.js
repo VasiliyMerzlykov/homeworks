820c1a77fdec95a5325d18b55bf3961255f9d737
Vasiliy Merzlyakov <V.Merov@yandex.ru>
2020-07-06 14:48:49 +0500

    third try second task

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,28 +1,32 @@
 function getResult(a,b,c){
     "use strict";
     let x =[];
-    let d = b**2 - 4*a*c
+    let d = b**2 - 4*a*c;
     if(d === 0){
         x.push((-b + Math.sqrt(d))/(2*a))
+        console.log(x)        
     } else if(d > 0){
         x.push((-b + Math.sqrt(d))/(2*a))
         x.push((-b - Math.sqrt(d))/(2*a))
-    } else return x;
+        console.log(x)        
+    };
+    console.log(x)
+    return x;
 }
 
 
 
-//                ***SECOND TRY SECOND TASK*** КОД СТАЛ КОРОЧЕ И ЧИТАБЕЛЬНЕЕ !!! СПАСИБО
+//                ***THIRD TRY SECOND TASK*** КОД СТАЛ КОРОЧЕ И ЧИТАБЕЛЬНЕЕ !!! СПАСИБО
 function getAverageMark(marks){   
     let sumScores = 0; 
     if(marks.length === 0){
-       return a;
-    } else if(marks.length){
-      marks.splice(5)
-      for(let i = 0; i <= marks.length - 1; i++){   
-        sumScores = marks[i] + a
-      }
-    };    
+       return 0;
+    } 
+    marks.splice(5);
+    for(let i = 0; i < marks.length; i++){   // 
+        sumScores += marks[i]
+    };
+        
     return (sumScores / marks.length).toFixed(1);
 }
 
