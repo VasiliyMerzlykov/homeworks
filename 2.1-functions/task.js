c8133e440c6fc7e5182fcf51c39a44126f67a6ac
Vasiliy Merzlyakov <V.Merov@yandex.ru>
2020-07-08 21:56:20 +0500

    add second task

--- 2.1-functions/task.js
+++ 2.1-functions/task.js
@@ -1,3 +1,4 @@
+// FIRST TASK
 function getSolutions( a, b, c ){
     let d = b**2 - 4*a*c;
     if(d < 0){
@@ -25,3 +26,50 @@ function showSolutionsMessage( a, b, c ){
       console.log(`«Уравнение имеет два корня. X₁ = ${result.roots[0]}, X₂ = ${result.roots[1]}»`)
     }
 }
+
+//SECOND TASK
+
+let array = {
+  alg: [1,3,5,6],
+  geo: [5,4,3]
+}
+function getSum(marks){//принимает объект
+  let sum = 0;// переменная для сложения оценок
+  for( let i = 0; i < marks.length; i++){//перебираем массив
+    sum += marks[i] /marks.length // складываем значения массива и делим на длину
+  }
+ return sum //Возвращаем результат
+}
+function getAverageScore(data){//принимает объект
+  let obj = {};
+  let sumAverage = 0;
+  let i = 0;
+  let allAverage = 0;
+  for(key in data){//перебираем объект 
+    ++i
+    obj[key] = getSum(data[key])
+    sumAverage += obj[key]
+    allAverage = sumAverage/i
+    obj.average = allAverage
+  }
+  return obj  
+}
+
+// второй вариант второй функции
+// function getAverageScore(data){//принимает объект
+//   let obj = {};
+//   let sumAverage = 0;
+//   let i = Object.keys(array);
+//   let allAverage = 0;
+//   for(key in data){//перебираем объект
+//     obj[key] = getSum(data[key])
+//     sumAverage += obj[key]
+//     allAverage = sumAverage/Object.keys(array).length
+//     obj.average = allAverage
+//   }
+//   return obj  
+// }
+
+// getAverageScore(array)
+
+//THIRD TASK
