2fb45f55cccf74d4c493f47d73281214b0f64c66
Vasiliy Merzlyakov <V.Merov@yandex.ru>
2020-07-09 13:41:13 +0500

    second try

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,21 +1,26 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
     "use ctrict"
     
-    /*1.тело кредита*/creditBody = amount - contribution
-    console.log("Тело кредита.: " + creditBody)
-    /*2.срок кредита*/loanРeriod = Math.floor((date - new Date())/(1000*60 * 60 * 24 * 30)) //не пойму как правильно посчитать раздницу между датами и соответственно количества месяцев
-    console.log("Срок кредита мес.: " +loanРeriod)
-    /*3.платеж*/monthlyPayment = creditBody*(percent + percent/(((1+percent)**loanРeriod)))
-    console.log("Платеж мес.: " + monthlyPayment)
-    /*4.общая сумма выплаты*/totalAmount = monthlyPayment * loanРeriod
-    console.log("Общаяя сумма выплаты: " + totalAmount.toFixed(2))
-    // код для задачи №1 писать здесь
-    return (totalAmount.toFixed(2)/100);// в консоле считает верно, в калькуляторе - нет, не понимаю почему
+    let creditBody = amount - contribution    
+    let loanРeriod = Math.floor((date - new Date())/(1000*60 * 60 * 24 * 30))     
+    let monthlyPayment = creditBody*(percent + percent/(((1+percent)**loanРeriod)))    
+    let totalAmount = monthlyPayment * loanРeriod
+    if(isNaN(+percent)){
+      return `Параметр <Процентная ставка> содержит неправильное значение <${percent}>`;
+    } ;
+    if(isNaN(+contribution)){
+      return `Параметр <Начальный взнос> содержит неправильное значение <${contribution}>`;
+    } ;
+    if(isNaN(+amount)) {
+      return `Параметр <Общая стоймость> содержит неправильное значение <${amount}>`;
+    };
+        
+    return (totalAmount.toFixed(2)/100);
 }
 
 function getGreeting(name) {
     let greeting;
     if (name !== "undefined" && name.length > 0) {
   return greeting = `Привет, мир! Меня зовут ${name}`;
-}else {return greeting = `Аноним`}     
-}// не могу решить, в любом случает получается, что name не пустой, а 
+}else {return greeting = "Привет, мир! Меня зовут Аноним"}     
+}
--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,5 +1,19 @@
 //String.prototype.isPalindrome - для задачи №1
 
+function Vehicle(n){
+    this.numbers=n;
+}
+Vehicle.getNumber=function(){
+    returnthis.number;
+}
+function Car(){
+    this.number=4;
+}
+function Bus(){
+    this.number=6;
+}
+Car.prototype=Vehicle;
+Bus.prototype=Vehicle;
 function getAverageMark(marks) {
     // код для задачи №2 писать здесь
     // return averageMark
