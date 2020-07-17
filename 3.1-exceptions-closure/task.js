f9e74f2c1c1e2d3caa5eb89c5873c267eaf1cc36
Vasiliy Merzlyakov <V.Merov@yandex.ru>
2020-07-17 13:38:41 +0500

    second try

--- 3.1-exceptions-closure/task.js
+++ 3.1-exceptions-closure/task.js
@@ -1,18 +1,19 @@
 // thirst task
 
 function parseCount(num){
-    num = parseInt(num)
-    if(isNaN(num)){    
+    const a = parseInt(num)
+    
+    if(isNaN(a)){    
     throw  new Error("Невалидное значение")
     };
-    return num;
+    return a;
 };
 
 function validateCount(needParse){
     try{
         return parseCount(needParse);
     } catch (e){
-        return e.message
+        return e
     };
 };
 
@@ -23,7 +24,7 @@ class Triangle{
         this.a = a;
         this.b = b;
         this.c = c;                   
-        if ((this.a + this.b) < this.c || (this.a + this.c) < this.b || (this.b + this.c) < this.a){
+        if ((a + b) < c || (a + c) < b || (b + c) < a){
             throw new Error("Треугольник с такими сторонами не существует")               
          } 
         };
@@ -34,30 +35,28 @@ class Triangle{
             let p = (this.a + this.b + this.c)/2            
             return Math.sqrt(p*(p - this.a)*(p - this.b)*(p - this.c)).toFixed(2)
         };
-        
+
 }    
 
 function getTriangle(a, b, c){
-    this.a = a;
-    this.b = b;
-    this.c = c;
+    
     try{        
-        return new Triangle(this.a, this.b = b, this.c = c);
+        return new Triangle(a, b = b, c = c);
     }catch(e){
+        const falseTriangle = {
+            getArea(){
+                return "Ошибка! Треугольник не существует"
+            },
+            getPerimeter(){
+                return "Ошибка! Треугольник не существует"
+            }
+            
+
+        }
+        return falseTriangle
         // не могу понять как релизовать =>  * В случае перехвата исключения возвращайте объект с двумя методами `getArea` и `getPerimeter`, которые возвращают строку: *"Ошибка! Треугольник не существует"*.
 
-        return new Triangle("Ошибка! Треугольник не существует")
+        
     };
-                
+
 }
- 
-
-
-
-
-
-
-  
-  
-  
-  
