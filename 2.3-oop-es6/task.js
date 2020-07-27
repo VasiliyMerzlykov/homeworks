7c9cad0f8519ef20332700f95f16cdce799c6d5a
Vasiliy Merzlyakov <V.Merov@yandex.ru>
2020-07-27 14:55:28 +0500

    final commit, i hope)))

--- 2.3-oop-es6/task.js
+++ 2.3-oop-es6/task.js
@@ -139,7 +139,8 @@ class Library{
   giveBookByName(bookName){
     for(let i = 0; i < this.books.length; i++){
     if(bookName === this.books[i].name ){
-      return this.books.splice(i, 1)
+      this.books.splice(i, 1)
+      return this.books[i]
     }
     }
   }
@@ -156,6 +157,6 @@ console.log(library.findBookBy("name", "Властелин колец")); //null
 console.log(library.findBookBy("releaseDate", 1924).name); //"Мурзилка"
 
 console.log("Количество книг до выдачи: " + library.books.length); //Количество книг до выдачи: 4
-library.giveBookByName("Машина времени");
+console.log( library.giveBookByName("Машина времени"));
 console.log("Количество книг после выдачи: " + library.books.length); //Количество книг после выдачи: 3
 
